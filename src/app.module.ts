454a858f8ff29477647877ddf63e5ef46d9267d4
Jardson Alan <jardsonalan132@gmail.com>
2026-09-14 13:43:04 -0300

    feat(module): Adiciona ConversasModule ao AppModule para integrar funcionalidades de gerenciamento de conversas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ChamadosModule } from './ia/chamados/chamados.module';
 import { IaModule } from './ia/ia.module';
+import { ConversasModule } from './conversas/conversas.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { IaModule } from './ia/ia.module';
     }),
     IaModule,
     ChamadosModule,
+    ConversasModule,
   ],
 })
 export class AppModule {}
