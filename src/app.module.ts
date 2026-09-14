e5e7533b2ac588733f01243edb6da812528ec971
Jardson Alan <jardsonalan132@gmail.com>
2026-09-14 13:24:26 -0300

    feat(docker): Adiciona serviços de backend e frontend as configurações do Docker Compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ChamadosModule } from './chamados/chamados.module';
+import { ChamadosModule } from './ia/chamados/chamados.module';
 import { IaModule } from './ia/ia.module';
 
 @Module({
--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,11 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: ['POST'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
